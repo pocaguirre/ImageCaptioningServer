d2b47eb83521bf8145d0046bdc703e2633f487e7
Carlos Aguirre <caguirr4@jhu.edu>
2022-05-08 18:35:21 -0400

    Updated app with calibration

--- inperson.py
+++ inperson.py
@@ -8,7 +8,7 @@ from multiprocessing import Value
 
 
 inperson = Blueprint('inperson', __name__)
-engine =  InPersonEngine(debug=True)
+engine =  InPersonEngine()
 assignment_id = Value('i', 0)
 
 
@@ -30,31 +30,14 @@ def inperson_submit():
     demographics = request.form['demographics']
     assignment_id = request.form['assignmentID']
     worker_id = request.form['workerID']
-    # extra = {k: v for k,v in request.form.items() if k not in ['answer', 'assignmentID', 'workerID', 'mturk', 'demographics']}
-    success = engine.save_data(worker_id, assignment_id, condition, answer, demographics)
+    calibrations = request.form['calibrations']
+    success = engine.save_data(worker_id, assignment_id, condition, answer, demographics, calibrations)
     return jsonify(success=success)
-    # if success:
-    #     if mturk_type not in current_app.config['MTURK_LINKS']:
-    #         return jsonify({"link": "Error Link, wrong data provided: MTURK_TYPE"})
-    #     else:
-    #         if mturk_type == 'azure':
-    #             if current_app.config['engine']._is_test_worker(worker_id):
-    #                 return jsonify({"link": "/test"})
-    #             if not current_app.config['engine']._check_valid_worker(worker_id):
-    #                 return jsonify({"link": "done"})
-    #             return jsonify({"link": f"/?worker={worker_id}"})
-    #         else:
-    #             return jsonify({"link": current_app.config['MTURK_LINKS'][mturk_type]})
-    # return
+
 
 @inperson.route('/inperson/get_images', methods=['POST'])
 def inperson_get_images():
     worker_id = request.form['workerID']
     images = engine.get_images(worker_id)
     return jsonify(images)
-
-@inperson.route('/inperson/dump/<table>', methods=['GET'])
-def inperson_dump(table):
-    result = engine.dump_table(table)
-    return jsonify(result)
 
